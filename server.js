dee610f6e634b2b0a38efbd2772551d9d096bd0b
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-08-10 13:23:52 -0600

    pre text work

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-// Dependencies
+'use strict'
+
+//Dependencies
+///////////////
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
-//Initializing the server
+/////////////////////////////
+// Initializing the server //
+/////////////////////////////
 var port = process.argv[2] || 8100;
 app.listen(port, function(err){
 	if(err){
@@ -13,4 +19,29 @@ app.listen(port, function(err){
 	else {
 		console.log("Listening on port " + port);
 	}
+});
+
+////////////////
+// Middleware //
+////////////////
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+///////////////
+// Endpoints //
+///////////////
+var message = {
+	"message": "Hello World"
+};
+
+app.get('/api/messages', function(req, res) {
+	return res.send(message);
+});
+app.post('/api/receive_message', function(req, res){
+	console.log(req.body);
+	res.send();
 });
